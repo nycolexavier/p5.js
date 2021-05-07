0ad88d1790d11676f6a67f3d74d78a80f1bb2af2
Nycole <nycole.xavierr@gmail.com>
2021-05-07 14:22:42 -0300

    map

---
+++ map.js
@@ -0,0 +1,16 @@
+var r = 0;
+var b = 255;
+
+function setup() {
+    createCanvas (600, 400);
+}
+
+function draw() {
+    // background
+    r = map(mouseX, 0, 600, 0, 255)
+    b = map(mouseX, 0, 600, 255, 0)
+    background(r, 0, b);
+    //ellipse
+    fill(250, 118, 222);
+    ellipse(mouseX, 200, 64, 64);
+}

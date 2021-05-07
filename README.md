8ffa58e1de9d4f3631425334bf0706848b28db5b
Nycole <nycole.xavierr@gmail.com>
2021-05-07 07:53:09 -0300

    primeira aula

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # p5.js
+
+## Lidando com figuras 2D
+
+(colocar um gif da atividade que está na pasta sketch.js)
  
---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220, 0, 200);
+
+  rectMode(CENTER);
+  rect(200, 150, 150, 150);
+}
